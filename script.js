809242c3a0d6dee16d636b4ed4e445c95c58d28b
Soumyadeep Ghosh <soumyadeepg27@gmail.com>
2023-05-09 12:42:20 +0530

    Updated

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ ScrollReveal({
     delay: 200 
 });
 
-ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
+ScrollReveal().reveal('.home-content', { origin: 'top'});
 ScrollReveal().reveal('.home-img, .experience-container, .skills-container, .portfolio-box, .contact form', { origin: 'bottom'});
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left'});
 ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right'});
